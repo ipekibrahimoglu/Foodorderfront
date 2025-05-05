766fe361a5aaa516ce317caa602bdcde027af1e2
ipek ibrahimoğlu <ipekibrahimogluu@gmail.com>
2025-05-05 13:19:17 +0300

    add orderitem

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,13 @@ import { RouterOutlet } from '@angular/router';
 import { NaviComponent } from "./components/navi/navi.component";
 import { CategoryComponent } from "./components/category/category.component";
 import { ProductComponent } from "./components/product/product.component";
-import { OrderItemsComponent } from "./components/order/orderItems.components";
 import { MenuComponent } from "./components/menu/menu.component";
+import { OrderComponent } from './components/order/order.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,                                  // ✅ eklendi
-  imports: [RouterOutlet, CommonModule, NaviComponent, CategoryComponent, ProductComponent, OrderItemsComponent, MenuComponent],             // ✅ CommonModule eklendi
+  imports: [RouterOutlet, CommonModule, NaviComponent, CategoryComponent, ProductComponent, OrderComponent, MenuComponent],             // ✅ CommonModule eklendi
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']                 // ✅ düzeltildi
 })
